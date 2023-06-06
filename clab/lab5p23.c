3cf95d684329947708f2c99d35df6f999d0af254
ARIF AKHTAR <120799230+arifakhtar51@users.noreply.github.com>
2023-06-06 21:33:14 +0530

    Update lab5p23.c

--- clab/lab5p23.c
+++ clab/lab5p23.c
@@ -14,3 +14,21 @@ int main(){
     }
     printf("The Binary Number of %d is %d",org,bin);
 }
+
+//by c++
+int main(){
+string ans;
+   int n=10;//or take input by cin>>n;
+   
+    while(n){//jab tak n 0 nahi ho jata chlo bhai
+        if(n&1)//agar nki phli bit 1 hai to 1 string me daal do for eg: n=8 in binary 1000;
+        ans.push_back('1');
+        else{
+           ans.push_back('0');
+        }
+        n=n>>1;//if n=8 in binary 1000; then n=n>>1 in binary 0100 that is 4 in decimal
+    }
+  reverse(ans.begin(),ans.end());
+  cout<<ans;
+  return 0;
+  }
